340b69717a5cbf25362ad22c3f66269e9b693f1c
DanoSvK <d.kopac@gmail.com>
2023-12-03 16:49:50 +0100

    Refactoring the code

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,8 +1,9 @@
+// Rendering game board
 const gameBoard = (() => {
-  const gameBoard = ["", "", "", "", "", "", "", "", ""];
   const render = () => {
     document.querySelector(".grid").style.display = "grid";
     document.querySelector(".btn-pvp").style.display = "none";
+    document.querySelector(".points").style.display = "block";
   };
   return { render };
 })();
@@ -25,129 +26,149 @@ const createPlayers = (name, mark) => {
   };
 };
 
-const gameStart = () => {
-  let isWin = false;
-  const imgX = '<img class="img-x" src="./x.svg" />';
-  const imgO = '<img class="img-o" src="./o.svg" />';
-  let gameWon = false;
-  let currPlayer = 0;
-  let arr = [[], []];
-  let playerPoints = [[], []];
-  let arrWin = [];
-  let rounds = 5;
-  let i = 1;
-  const players = [
-    createPlayers("Player 1", imgX),
-    createPlayers(`Player 2`, imgO),
+// Main object
+class GameStart {
+  grid = document.querySelector(".grid");
+  nextRound = document.querySelector(".next-round-btn");
+  restart = document.querySelector(".restart-btn");
+  mainMenu = document.querySelectorAll(".main-menu");
+  startBtn = document.querySelector(".btn-pvp");
+  winRoundScreen = document.querySelector(".win-screen");
+  winGameScreen = document.querySelector(".win-game");
+  points = document.querySelector(".points");
+  isWin = false;
+  imgX = '<img class="img-x" src="./x.svg" />';
+  imgO = '<img class="img-o" src="./o.svg" />';
+  gameWon = false;
+  currPlayer = 0;
+  arr = [[], []];
+  playerPoints = [[], []];
+  arrWin = [];
+  rounds = 5;
+  i = 1;
+  players = [
+    createPlayers("Player 1", this.imgX),
+    createPlayers(`Player 2`, this.imgO),
   ];
 
-  const handleWin = (array, player) => {
-    return winConditions.some((condition) => {
-      isWin = condition.every((index) => array[player].includes(index));
-      if (isWin) {
-        arrWin = condition;
-      }
-      return isWin;
-    });
-  };
+  constructor() {
+    this.startBtn.addEventListener("click", gameBoard.render);
+    this.grid.addEventListener("click", this.handlePlay.bind(this));
+    this.nextRound.addEventListener("click", this.handleNextRound.bind(this));
+    this.restart.addEventListener("click", this.handleRestart.bind(this));
+    this.mainMenu.forEach((btn) =>
+      btn.addEventListener("click", this.handleMainMenu.bind(this))
+    );
+  }
 
-  const handleWinningScreen = (player) => {
-    if (rounds === i) {
-      document.querySelector(".win-game").style.display = "block";
+  handleWin(array, player) {
+    return winConditions.some((condition) => {
+      this.isWin = condition.every((index) => array[player].includes(index));
+      if (this.isWin) {
+        this.arrWin = condition;
+      }
+      return this.isWin;
+    });
+  }
+
+  handleWinningScreen(player) {
+    // Win round screen
+    if (this.rounds === this.i) {
+      this.winGameScreen.style.display = "block";
       document.querySelector(".win-game-modal").innerHTML = `<p>Player ${
         player + 1
       } won the game!`;
       return;
     }
-    i++;
-    document.querySelector(".win-screen").style.display = "block";
+    // Win game screen
+    this.i++;
+    this.winRoundScreen.style.display = "block";
     document.querySelector(".win-modal").innerHTML = `<p>Player ${
       player + 1
-    } won round ${i - 1}`;
+    } won round ${this.i - 1}`;
+  }
+
+  handleDataReset = () => {
+    this.arr = [[], []];
+    this.currPlayer = 0;
+    this.arrWin = [];
+    this.isWin = false;
+    this.gameWon = false;
   };
 
-  const handleRestart = () => {
-    arr = [[], []];
-    currPlayer = 0;
-    arrWin = [];
-    isWin = false;
-    gameWon = false;
-  };
+  highlightWinningFields(winningCombination) {
+    for (const index of winningCombination) {
+      const field = document.querySelector(`.field[data-id="${index}"]`);
+      field.classList.add("winner");
+    }
+  }
 
-  gameBoard.render();
-  document.querySelector(".grid").addEventListener("click", (e) => {
-    console.log(arr);
-    if (gameWon) {
+  handlePlay(e) {
+    console.log(this.arr.length);
+    if (this.gameWon) {
       return;
     }
-    if (e.target.classList.contains("field")) {
-      if (e.target.innerHTML == "") {
-        e.target.innerHTML = players[currPlayer].mark;
-        arr[currPlayer].push(+e.target.dataset.id);
-        if (handleWin(arr, currPlayer)) {
-          highlightWinningFields(arrWin);
-          handleWinningScreen(currPlayer);
-          playerPoints[currPlayer]++;
-          document.querySelector(`.p${currPlayer + 1}`).innerHTML =
-            playerPoints[currPlayer];
-          console.log(playerPoints[currPlayer]);
-          gameWon = true;
-        } else {
-          currPlayer = currPlayer == 0 ? 1 : 0;
-        }
+    if (e.target.classList.contains("field") && e.target.innerHTML == "") {
+      e.target.innerHTML = this.players[this.currPlayer].mark;
+      this.arr[this.currPlayer].push(+e.target.dataset.id);
+      if (this.handleWin(this.arr, this.currPlayer)) {
+        this.highlightWinningFields(this.arrWin);
+        this.handleWinningScreen(this.currPlayer);
+        this.playerPoints[this.currPlayer]++;
+        document.querySelector(`.p${this.currPlayer + 1}`).innerHTML =
+          this.playerPoints[this.currPlayer];
+        this.gameWon = true;
       } else {
-        return;
+        this.currPlayer = this.currPlayer == 0 ? 1 : 0;
       }
+    } else {
+      return;
     }
-  });
+    if (this.arrWin.length == 9) {
+      console.log("draw");
+    }
+  }
 
-  document.querySelector(".next-round-btn").addEventListener("click", () => {
+  handleNextRound() {
     document.querySelectorAll(".field").forEach((el) => {
       el.innerHTML = "";
       el.classList.remove("winner");
     });
     document.querySelector(".win-screen").style.display = "none";
-    handleRestart();
-  });
+    this.handleDataReset();
+  }
 
-  document.querySelector(".restart-btn").addEventListener("click", () => {
-    i = 1;
+  resetPlayerPoint() {
+    this.playerPoints = [[], []];
+    document.querySelector(".p1").innerHTML = 0;
+    document.querySelector(".p2").innerHTML = 0;
+  }
+
+  handleRestart() {
+    this.i = 1;
     document.querySelectorAll(".field").forEach((el) => {
       el.innerHTML = "";
       el.classList.remove("winner");
     });
     document.querySelector(".win-game").style.display = "none";
-    handleRestart();
-    playerPoints = [[], []];
-    document.querySelector(".p1").innerHTML = 0;
-    document.querySelector(".p2").innerHTML = 0;
-  });
+    this.handleDataReset();
+    this.resetPlayerPoint();
+  }
 
-  document.querySelectorAll(".main-menu").forEach((btn) =>
-    btn.addEventListener("click", () => {
-      i = 1;
-      document.querySelectorAll(".field").forEach((el) => {
-        el.innerHTML = "";
-        el.classList.remove("winner");
-      });
-      document.querySelector(".grid").style.display = "none";
-      document.querySelector(".win-screen").style.display = "none";
-      document.querySelector(".win-game").style.display = "none";
-      document.querySelector(".btn-pvp").style.display = "block";
-      handleRestart();
-      playerPoints = [[], []];
-      document.querySelector(".p1").innerHTML = 0;
-      document.querySelector(".p2").innerHTML = 0;
-    })
-  );
+  handleMainMenu() {
+    this.i = 1;
+    document.querySelectorAll(".field").forEach((el) => {
+      el.innerHTML = "";
+      el.classList.remove("winner");
+      this.grid.style.display = "none";
+      this.winRoundScreen.style.display = "none";
+      this.winGameScreen.style.display = "none";
+      this.startBtn.style.display = "block";
+      this.points.style.display = "none";
+      this.handleDataReset();
+      this.resetPlayerPoint();
+    });
+  }
+}
 
-  const highlightWinningFields = (winningCombination) => {
-    for (const index of winningCombination) {
-      const field = document.querySelector(`.field[data-id="${index}"]`);
-      field.classList.add("winner");
-    }
-  };
-};
-
-document.querySelector(".btn-pvp").addEventListener("click", gameStart);
-// document.querySelector(".restart-btn").addEventListener("click", gameStart);
+document.querySelector(".btn-pvp").addEventListener("click", new GameStart());
