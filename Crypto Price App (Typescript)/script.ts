704c5cb1d8e1e0c77f4d4f79f90b7a58f65363ec
Daniel <d.kopac@gmail.com>
2024-01-08 18:32:30 +0100

    Created functional sorting, pagination and finished the app's logic

--- Crypto Price App (Typescript)/script.ts
+++ Crypto Price App (Typescript)/script.ts
@@ -1,55 +1,164 @@
-// // const price =
-// //   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_change=true&include_last_updated_at=true&precision=2";
-
-// // const change =
-// //   "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30";
-
-// // const coinsList = "https://api.coingecko.com/api/v3/coins/list";
-
-// // const allCoins =
-// //   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en&precision=2";
-
 class CryptoApp {
+  // Selectors
+  paginationEl = document.querySelector(".pagination") as HTMLDivElement;
+
+  // Logic
+  currPage = 1;
+  items = 15;
+  itemsPerPage = 3;
+  isPriceAscending = true;
+  isMarketCapAscending = true;
+  is24hChangeAscending = true;
+  fetchedData: [] = [];
   constructor() {
     this.getData();
+    this.paginationEl.addEventListener("click", (e) => {
+      this.getPageData(e);
+    });
+
+    document
+      .querySelector(".btn-market-cap")!
+      .addEventListener("click", this.sortMarketCap.bind(this));
+
+    document
+      .querySelector(".btn-price")!
+      .addEventListener("click", this.sortPrice.bind(this));
+
+    document
+      .querySelector(".btn-24h-change")!
+      .addEventListener("click", this.sort24hChange.bind(this));
+
+    document
+      .querySelectorAll(".btn")
+      .forEach((arrow) =>
+        arrow.addEventListener("click", this.handleSortArrows)
+      );
   }
 
-  sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  getPage(page: number, data: any) {
-    const start = (page - 1) * 10;
-    const end = page * 10;
+  // Slicing data items from array based on the current page
+  getPage(data: any, page: number = this.currPage) {
+    this.currPage = page;
+    const start = (page - 1) * this.itemsPerPage;
+    const end = page * this.itemsPerPage;
     return data.slice(start, end);
   }
 
-  getData = (): void => {
-    const itemsPerPage = 250;
-    const rateLimitDelay = 3000;
-    let page = 1;
-    fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${itemsPerPage}
-      
-      &page=${page}&sparkline=false&locale=en&precision=2`
-    )
-      .then((res: Response) => res.json())
-      .then((data: any) => {
-        console.log(this.getPage(2, data));
-        // page++;
-        // data.map((el: any) => {
-        //   document.querySelector(".list")!.innerHTML += this.markup(el);
-        // });
+  getPageData(e: Event) {
+    const buttonElement = e.target as HTMLButtonElement;
+    const closestBtnElement = buttonElement.closest(
+      ".btn"
+    ) as HTMLElement | null;
 
-        // return this.sleep(rateLimitDelay).then(this.getData);
-      });
+    if (closestBtnElement) {
+      const gotoValue = closestBtnElement.dataset.goto;
+
+      if (gotoValue) {
+        this.currPage = +gotoValue;
+        this.getData();
+      } else {
+        console.log('Dataset property "goto" not found');
+      }
+    } else {
+      console.log('Ancestor element with class "btn" not found');
+    }
+  }
+
+  getData = (): void => {
+    const fetchData = (): void => {
+      fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${this.items}&page=1&sparkline=false&locale=en&precision=2`
+      )
+        .then((res: Response) => res.json())
+        .then((data: any) => {
+          this.fetchedData = data;
+          this.renderData(data);
+          console.log(data);
+        });
+    };
+    fetchData();
   };
 
+  sortPrice(e: Event) {
+    e.preventDefault();
+    this.currPage = 1;
+    this.fetchedData.sort((a: any, b: any) => {
+      return this.isPriceAscending
+        ? a.current_price - b.current_price
+        : b.current_price - a.current_price;
+    });
+    this.isPriceAscending = !this.isPriceAscending;
+
+    this.renderData(this.fetchedData);
+  }
+
+  sortMarketCap(e: Event) {
+    e.preventDefault();
+    this.currPage = 1;
+    this.fetchedData.sort((a: any, b: any) => {
+      return this.isMarketCapAscending
+        ? a.market_cap - b.market_cap
+        : b.market_cap - a.market_cap;
+    });
+    this.isMarketCapAscending = !this.isMarketCapAscending;
+
+    this.renderData(this.fetchedData);
+  }
+
+  sort24hChange(e: Event) {
+    e.preventDefault();
+    this.currPage = 1;
+    this.fetchedData.sort((a: any, b: any) => {
+      return this.is24hChangeAscending
+        ? a.price_change_percentage_24h - b.price_change_percentage_24h
+        : b.price_change_percentage_24h - a.price_change_percentage_24h;
+    });
+    this.is24hChangeAscending = !this.is24hChangeAscending;
+
+    this.renderData(this.fetchedData);
+  }
+
+  // Rendering markups method
+  renderData(data: any) {
+    const listEl = document.querySelector(".list") as HTMLDivElement;
+    // Slicing data
+    const pagesOfData = this.getPage(data, this.currPage);
+    this.paginationEl.innerHTML = this.paginationMarkup();
+
+    listEl.innerHTML = "";
+    pagesOfData.map((el: any) => {
+      listEl.innerHTML += this.markup(el);
+    });
+  }
+
+  handleSortArrows(e: Event) {
+    const arrows = document.querySelectorAll(
+      ".arrow"
+    ) as NodeListOf<HTMLSpanElement>;
+    /* Reset arrow to default (invisible + 0 deg). 
+    Rotate-arrow class set to important to overwrite inline-style */
+    arrows.forEach((arrow) => {
+      arrow.classList.add("inactive");
+      arrow.classList.add("rotate-arrow");
+    });
+
+    /* Accessing respective arrow, toggling inactive to be always visible
+     and removing rotate-arrow to make inline style overwrite class */
+    const target = e.target as HTMLSpanElement;
+    target.querySelector(".arrow")?.classList.toggle("inactive");
+    target.querySelector(".arrow")?.classList.remove("rotate-arrow");
+    const arrowElement = target.querySelector(".arrow") as HTMLSpanElement;
+
+    // Toggling between 0deg and 180deg
+    if (arrowElement.style.transform == "rotate(180deg)") {
+      arrowElement.style.transform = "rotate(0deg)";
+    } else {
+      arrowElement.style.transform = "rotate(180deg)";
+    }
+  }
+
+  // Main data markup method
   markup(data: any) {
-    const priceChange: number = +(
-      (100 / data.current_price) *
-      data.price_change_24h
-    ).toFixed(2);
+    const priceChange: number = data.price_change_percentage_24h.toFixed(2);
     return `<div class="coin">
         <div class="basic-info">
             <img
@@ -68,182 +177,39 @@ class CryptoApp {
         }">${isFinite(priceChange) ? priceChange : "0.00"} %</p>
     </div>`;
   }
+
+  // Pagination markup method
+  paginationMarkup(): string {
+    const totalPages = Math.ceil(this.items / this.itemsPerPage);
+    // First page and other pages
+    if (this.currPage === 1 && totalPages > 1) {
+      return `<button class="btn btn-next" data-goto="${
+        this.currPage + 1
+      }">Page ${this.currPage + 1} ></button>`;
+    }
+
+    // Last page
+    if (this.currPage === totalPages) {
+      return `<button class="btn btn-previous" data-goto="${
+        this.currPage - 1
+      }">< Page ${this.currPage - 1}</button>`;
+    }
+
+    // Other pages
+    if (this.currPage < totalPages) {
+      return `<button class="btn btn-previous" data-goto="${
+        this.currPage - 1
+      }">< Page ${this.currPage - 1}</button>
+      <button class="btn btn-next" data-goto="${this.currPage + 1}">Page ${
+        this.currPage + 1
+      }></button>`;
+    }
+
+    // First page and no other pages
+    else {
+      return "";
+    }
+  }
 }
 
 new CryptoApp();
-
-// Sorting functionality
-let isAscending = false;
-
-document.querySelector(".btn")!.addEventListener("click", (e) => {
-  e.preventDefault();
-  const arr = [
-    {
-      id: "bitcoin",
-      symbol: "btc",
-      name: "Bitcoin",
-      image:
-        "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
-      current_price: 42635,
-      market_cap: 833985155308,
-      market_cap_rank: 1,
-      fully_diluted_valuation: 894325830109,
-      total_volume: 23446834605,
-      high_24h: 43789,
-      low_24h: 42324,
-      price_change_24h: -371.60079922046134,
-      price_change_percentage_24h: -0.86405,
-      market_cap_change_24h: -7553907517.581665,
-      market_cap_change_percentage_24h: -0.89763,
-      circulating_supply: 19583118,
-      total_supply: 21000000,
-      max_supply: 21000000,
-      ath: 69045,
-      ath_change_percentage: -38.53111,
-      ath_date: "2021-11-10T14:24:11.849Z",
-      atl: 67.81,
-      atl_change_percentage: 62489.11535,
-      atl_date: "2013-07-06T00:00:00.000Z",
-      roi: null,
-      last_updated: "2023-12-28T18:33:30.495Z",
-    },
-    {
-      id: "ethereum",
-      symbol: "eth",
-      name: "Ethereum",
-      image:
-        "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
-      current_price: 2368.32,
-      market_cap: 284022533707,
-      market_cap_rank: 2,
-      fully_diluted_valuation: 284022533707,
-      total_volume: 29080628928,
-      high_24h: 2442.31,
-      low_24h: 2342.79,
-      price_change_24h: 20.9,
-      price_change_percentage_24h: 0.8905,
-      market_cap_change_24h: 2290214162,
-      market_cap_change_percentage_24h: 0.8129,
-      circulating_supply: 120183735.874744,
-      total_supply: 120183735.874744,
-      max_supply: null,
-      ath: 4878.26,
-      ath_change_percentage: -51.76559,
-      ath_date: "2021-11-10T14:24:19.604Z",
-      atl: 0.432979,
-      atl_change_percentage: 543344.62114,
-      atl_date: "2015-10-20T00:00:00.000Z",
-      roi: {
-        times: 73.31583796016828,
-        currency: "btc",
-        percentage: 7331.583796016827,
-      },
-      last_updated: "2023-12-28T18:33:29.343Z",
-    },
-    {
-      id: "ethereum",
-      symbol: "eth",
-      name: "Ethereum",
-      image:
-        "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
-      current_price: 236.32,
-      market_cap: 284022533707,
-      market_cap_rank: 2,
-      fully_diluted_valuation: 284022533707,
-      total_volume: 29080628928,
-      high_24h: 2442.31,
-      low_24h: 2342.79,
-      price_change_24h: 20.9,
-      price_change_percentage_24h: 0.8905,
-      market_cap_change_24h: 2290214162,
-      market_cap_change_percentage_24h: 0.8129,
-      circulating_supply: 120183735.874744,
-      total_supply: 120183735.874744,
-      max_supply: null,
-      ath: 4878.26,
-      ath_change_percentage: -51.76559,
-      ath_date: "2021-11-10T14:24:19.604Z",
-      atl: 0.432979,
-      atl_change_percentage: 543344.62114,
-      atl_date: "2015-10-20T00:00:00.000Z",
-      roi: {
-        times: 73.31583796016828,
-        currency: "btc",
-        percentage: 7331.583796016827,
-      },
-      last_updated: "2023-12-28T18:33:29.343Z",
-    },
-    {
-      id: "ethereum",
-      symbol: "eth",
-      name: "Ethereum",
-      image:
-        "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
-      current_price: 4368.32,
-      market_cap: 284022533707,
-      market_cap_rank: 2,
-      fully_diluted_valuation: 284022533707,
-      total_volume: 29080628928,
-      high_24h: 2442.31,
-      low_24h: 2342.79,
-      price_change_24h: 20.9,
-      price_change_percentage_24h: 0.8905,
-      market_cap_change_24h: 2290214162,
-      market_cap_change_percentage_24h: 0.8129,
-      circulating_supply: 120183735.874744,
-      total_supply: 120183735.874744,
-      max_supply: null,
-      ath: 4878.26,
-      ath_change_percentage: -51.76559,
-      ath_date: "2021-11-10T14:24:19.604Z",
-      atl: 0.432979,
-      atl_change_percentage: 543344.62114,
-      atl_date: "2015-10-20T00:00:00.000Z",
-      roi: {
-        times: 73.31583796016828,
-        currency: "btc",
-        percentage: 7331.583796016827,
-      },
-      last_updated: "2023-12-28T18:33:29.343Z",
-    },
-    {
-      id: "ethereum",
-      symbol: "eth",
-      name: "Ethereum",
-      image:
-        "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
-      current_price: 3368.32,
-      market_cap: 284022533707,
-      market_cap_rank: 2,
-      fully_diluted_valuation: 284022533707,
-      total_volume: 29080628928,
-      high_24h: 2442.31,
-      low_24h: 2342.79,
-      price_change_24h: 20.9,
-      price_change_percentage_24h: 0.8905,
-      market_cap_change_24h: 2290214162,
-      market_cap_change_percentage_24h: 0.8129,
-      circulating_supply: 120183735.874744,
-      total_supply: 120183735.874744,
-      max_supply: null,
-      ath: 4878.26,
-      ath_change_percentage: -51.76559,
-      ath_date: "2021-11-10T14:24:19.604Z",
-      atl: 0.432979,
-      atl_change_percentage: 543344.62114,
-      atl_date: "2015-10-20T00:00:00.000Z",
-      roi: {
-        times: 73.31583796016828,
-        currency: "btc",
-        percentage: 7331.583796016827,
-      },
-      last_updated: "2023-12-28T18:33:29.343Z",
-    },
-  ];
-
-  isAscending
-    ? arr.sort((a: any, b: any) => a.current_price - b.current_price)
-    : arr.sort((a: any, b: any) => b.current_price - a.current_price);
-  console.log(arr.map((el) => el?.current_price));
-  isAscending = isAscending ? false : true;
-});
