5587aa021755cae5bbc863d46226db60fbf6d0a4
Daniel <d.kopac@gmail.com>
2024-01-25 22:35:58 +0100

    Testing function for rendering an object an object.

---
+++ Geometry generator/script.ts
@@ -0,0 +1,7 @@
+// const createCircle = () => {
+//   const element = document.createElement("div");
+//   element.classList.add("triangle");
+//   const appElement = document.querySelector(".app") as HTMLDivElement;
+//   appElement.innerHTML = element.outerHTML;
+// };
+// createCircle();
