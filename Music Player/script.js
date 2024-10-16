78a5ed4f043b17b3bd3c1ddfeadff0ea601b2e74
Daniel <d.kopac@gmail.com>
2024-10-16 21:30:42 +0200

    Finished most of the functionalities

--- Music Player/script.js
+++ Music Player/script.js
@@ -1,12 +1,47 @@
 var playBtn = document.querySelector(".btn-play");
 var audioPlayer = document.getElementById("audioPlayer");
-var volumeControl = document.getElementById("volumeControl");
+var volumeControl = document.getElementById("volumeControl"); //prettier-ignore
 var timeControl = document.getElementById("timeControl");
 var backwardBtn = document.querySelector(".fa-backward");
 var forwardBtn = document.querySelector(".fa-forward");
+var slider = document.querySelector('input[type="range"]'); //prettier-ignore
+var audioSource = document.querySelector("source");
+var backwardStepBtn = document.querySelector(".fa-backward-step"); //prettier-ignore
+var forwardStepBtn = document.querySelector(".fa-forward-step"); //prettier-ignore
+var imageContainer = document.querySelector(".image");
+var overlay = document.querySelector(".overlay");
+var songs = [
+    {
+        path: "./1.mp3",
+        title: "Motion",
+        author: "CardiB ft. Latto & Sexyy Red",
+        image: "./1.jpg",
+    },
+    {
+        path: "./2.mp3",
+        title: "Lose Youeself",
+        author: "Eminem",
+        image: "./2.jpg",
+    },
+    {
+        path: "./3.mp3",
+        title: "Eyes Closed",
+        author: "Imagine Dragons",
+        image: "./3.jpg",
+    },
+];
+var currSong = 0;
 window.addEventListener("load", function () {
-    timeControl.max = "".concat(audioPlayer.duration);
-    console.log(audioPlayer);
+    audioSource.src = songs[currSong].path;
+    imageContainer.style.backgroundImage = "url(".concat(songs[currSong].image, ")");
+    overlay.style.backgroundImage = "url(".concat(songs[currSong].image, ")");
+    console.log(songs[currSong].image);
+    // Make sure the audio is loaded
+    audioPlayer.load();
+    // Waiting for metadata to get duration
+    audioPlayer.addEventListener("loadedmetadata", function () {
+        timeControl.max = "".concat(audioPlayer.duration);
+    });
 });
 playBtn.addEventListener("click", function (e) {
     var isPaused = e.target.classList.contains("btn-play");
@@ -35,10 +70,46 @@ backwardBtn.addEventListener("click", function (e) {
 timeControl.addEventListener("input", function (e) {
     audioPlayer.currentTime = +e.target.value;
 });
-var slider = document.querySelector('input[type="range"]');
 audioPlayer.addEventListener("timeupdate", function () {
     timeControl.value = "".concat(audioPlayer.currentTime);
-    console.log(timeControl.max);
     var value = (audioPlayer.currentTime / audioPlayer.duration) * 100;
     timeControl.style.background = "linear-gradient(to right, #fff ".concat(value, "%, #ddd ").concat(value, "%)");
+});
+backwardStepBtn.addEventListener("click", function () {
+    if (currSong == 0) {
+        currSong = songs.length - 1;
+    }
+    else {
+        currSong -= 1;
+    }
+    audioSource.src = songs[currSong].path;
+    imageContainer.style.backgroundImage = "url(".concat(songs[currSong].image, ")");
+    overlay.style.backgroundImage = "url(".concat(songs[currSong].image, ")");
+    // Make sure the audio is loaded
+    audioPlayer.load();
+    // Waiting for metadata to get duration
+    audioPlayer.addEventListener("loadedmetadata", function () {
+        timeControl.max = "".concat(audioPlayer.duration);
+        console.log("Duration:", audioPlayer.duration); // Now this will print the correct duration
+    });
+    audioPlayer.play();
+});
+forwardStepBtn.addEventListener("click", function () {
+    if (currSong == songs.length - 1) {
+        currSong = 0;
+    }
+    else {
+        currSong += 1;
+    }
+    audioSource.src = songs[currSong].path;
+    imageContainer.style.backgroundImage = "url(".concat(songs[currSong].image, ")");
+    overlay.style.backgroundImage = "url(".concat(songs[currSong].image, ")");
+    // Make sure the audio is loaded
+    audioPlayer.load();
+    // Waiting for metadata to get duration
+    audioPlayer.addEventListener("loadedmetadata", function () {
+        timeControl.max = "".concat(audioPlayer.duration);
+        console.log("Duration:", audioPlayer.duration); // Now this will print the correct duration
+    });
+    audioPlayer.play();
 });
